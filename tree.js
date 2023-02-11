1d5d8f96c69484115be6f555e7605413e1892264
xsoc <69184503+xsoc@users.noreply.github.com>
2023-02-11 23:40:27 +1000

    Create tree.js

---
+++ tree.js
@@ -0,0 +1,31 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	var master_server_list = ["home"];
+	let depth = 0
+
+	spider("home");
+
+	function spider(target) {
+		depth++;
+		var server_list = ns.scan(target);
+		for (var server of server_list) {
+			if (master_server_list.indexOf(server) == -1) {
+				master_server_list.push(server)
+
+				let dash = ""
+				for (let i = 1; i < depth; i++) {
+					dash += "-"
+				}
+				
+				if (ns.hasRootAccess(server)) {
+					dash = "INFO " + dash
+				} else {
+					dash = "     " + dash
+				}
+				ns.tprint(dash + server)
+				spider(server);
+			}
+		}
+		depth--;
+	}
+}
