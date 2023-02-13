2ac1cd371580ea509db9a202ad20a9d8f40ef482
xsoc <69184503+xsoc@users.noreply.github.com>
2023-02-13 21:11:32 +1000

    Encryption II: Vigenère Cipher

--- contractor.js
+++ contractor.js
@@ -28,46 +28,18 @@ export async function main(ns) {
 			ns.tprint(data);
 
 			switch (ns.codingcontract.getContractType(contract, server)) {
-				case "Algorithmic Stock Trader I":
-					let max_profit = 0;
-					let best_buy = 0;
-					let best_sell = 0;
-
-					for (let i = 0; i < data.length; i++) {
-						for (let j = i + 1; j < data.length; j++) {
-							let profit = data[j] - data[i];
-
-							if (profit > max_profit) {
-								max_profit = profit;
-								best_buy = i;
-								best_sell = j;
-							}
-						}
-					}
-					ns.tprint("INFO " + ns.codingcontract.attempt(max_profit, contract, server));
+				case "Find Largest Prime Factor":
 					break;
-				case "Encryption I: Caesar Cipher":
-					const caeser_data = data[0]
-					const caeser_key = data[1]
-					answer = ""
 
-					for (let i = 0; i < caeser_data.length; i++) {
-						let letter = caeser_data[i]
-						if (letter == " ") {
-							answer += " "
-						} else {
-							let ascii_value = letter.charCodeAt(0) - caeser_key;
-							if (ascii_value < "A".charCodeAt(0)) ascii_value += 26
-							answer += String.fromCharCode(ascii_value)
-						}
-					}
-
-					ns.tprint("INFO " + ns.codingcontract.attempt(answer, contract, server));
+				case "Subarray with Maximum Sum":
 					break;
-				//case "Find Largest Prime Factor":
-				//case "Subarray with Maximum Sum":
-				//case "Total Ways to Sum":
-				//case "Total Ways to Sum II":
+
+				case "Total Ways to Sum":
+					break;
+
+				case "Total Ways to Sum II":
+					break;
+
 				case "Spiralize Matrix":
 					answer = [];
 					const vectors = [[1, 0], [0, 1], [-1, 0], [0, -1]];
@@ -110,22 +82,74 @@ export async function main(ns) {
 					ns.tprint("INFO " + ns.codingcontract.attempt(answer, contract, server))
 
 					break;
-				//case "Array Jumping Game":
-				//case "Array Jumping Game II":
-				//case "Merge Overlapping Intervals":
-				//case "Generate IP Addresses":
-				//case "Algorithmic Stock Trader II":
-				//case "Algorithmic Stock Trader III":
-				//case "Algorithmic Stock Trader IV":
-				//case "Minimum Path Sum in a Triangle":
-				//case "Unique Paths in a Grid I":
-				//case "Unique Paths in a Grid II":
-				//case "Shortest Path in a Grid":
-				//case "Sanitize Parentheses in Expression":
-				//case "Find All Valid Math Expressions":
-				//case "HammingCodes: Integer to Encoded Binary":
-				//case "HammingCodes: Encoded Binary to Integer":
-				//case "Proper 2-Coloring of a Graph":
+
+				case "Array Jumping Game":
+					break;
+
+				case "Array Jumping Game II":
+					break;
+
+				case "Merge Overlapping Intervals":
+					break;
+
+				case "Generate IP Addresses":
+					break;
+
+				case "Algorithmic Stock Trader I":
+					let max_profit = 0;
+					let best_buy = 0;
+					let best_sell = 0;
+
+					for (let i = 0; i < data.length; i++) {
+						for (let j = i + 1; j < data.length; j++) {
+							let profit = data[j] - data[i];
+
+							if (profit > max_profit) {
+								max_profit = profit;
+								best_buy = i;
+								best_sell = j;
+							}
+						}
+					}
+					ns.tprint("INFO " + ns.codingcontract.attempt(max_profit, contract, server));
+					break;
+
+				case "Algorithmic Stock Trader II":
+					break;
+
+				case "Algorithmic Stock Trader III":
+					break;
+
+				case "Algorithmic Stock Trader IV":
+					break;
+
+				case "Minimum Path Sum in a Triangle":
+					break;
+
+				case "Unique Paths in a Grid I":
+					break;
+
+				case "Unique Paths in a Grid II":
+					break;
+
+				case "Shortest Path in a Grid":
+					break;
+
+				case "Sanitize Parentheses in Expression":
+					break;
+
+				case "Find All Valid Math Expressions":
+					break;
+
+				case "HammingCodes: Integer to Encoded Binary":
+					break;
+
+				case "HammingCodes: Encoded Binary to Integer":
+					break;
+
+				case "Proper 2-Coloring of a Graph":
+					break;
+
 				case "Compression I: RLE Compression":
 					answer = ""
 					let current_letter = ""
@@ -148,9 +172,59 @@ export async function main(ns) {
 					ns.tprint(answer);
 					ns.tprint(ns.codingcontract.attempt(answer, contract, server))
 					break;
-				//case "Compression II: LZ Decompression":
-				//case "Compression III: LZ Compression":
-				//case "Encryption II: Vigenère Cipher"]:
+				case "Compression II: LZ Decompression":
+					break;
+
+				case "Compression III: LZ Compression":
+					break;
+
+				case "Encryption I: Caesar Cipher":
+					const caeser_data = data[0]
+					const caeser_key = data[1]
+					answer = ""
+
+					for (let i = 0; i < caeser_data.length; i++) {
+						let letter = caeser_data[i]
+						if (letter == " ") {
+							answer += " "
+						} else {
+							let ascii_value = letter.charCodeAt(0) - caeser_key;
+							if (ascii_value < "A".charCodeAt(0)) ascii_value += 26
+							answer += String.fromCharCode(ascii_value)
+						}
+					}
+
+					ns.tprint("INFO " + ns.codingcontract.attempt(answer, contract, server));
+					break;
+
+				case "Encryption II: Vigenère Cipher":
+					let message = data[0];
+					let key = data[1]
+					answer = ""
+					let alphabet = "";
+					let vigenereSquare = [];
+
+					for (let i = "A".charCodeAt(0); i <= "Z".charCodeAt(0); i++) alphabet += String.fromCharCode(i)
+
+					for (let i = 0; i < alphabet.length; i++) {
+						let row = [];
+						for (let j = 0; j < alphabet.length; j++) {
+							row.push(alphabet[(i + j) % alphabet.length]);
+						}
+						vigenereSquare.push(row);
+					}
+
+					for (let i = 0; i < message.length; i++) {
+						let letter = message[i];
+						let value = letter.charCodeAt(0) - "A".charCodeAt(0)
+						let key_index = answer.length % key.length
+						let key_value = alphabet.indexOf(key.charAt(key_index));
+						answer += vigenereSquare[value][key_value]
+					}
+
+					ns.tprint("INFO " + ns.codingcontract.attempt(answer, contract, server));
+
+					break;
 			}
 		}
 	}
