bad00b421399f9b5d1cee79341e5ac3ede4187b3
xsoc <69184503+xsoc@users.noreply.github.com>
2023-04-28 16:45:16 +1000

    Update bladeburner.js
    A lot of bug fixes

--- bladeburner.js
+++ bladeburner.js
@@ -1,53 +1,54 @@
 /** @param {NS} ns */
 export async function main(ns) {
-	var blackops = [];
-	var blackopNames = ns.bladeburner.getBlackOpNames();
-	for (const name of blackopNames) {
-		const rank = ns.bladeburner.getBlackOpRank(name);
-		blackops[name] = {rank: rank};
-	}
-
-	blackops.sort(function(a, b){
-		return b.rank - a.rank;
-	})
-
-	while(true) {
-		let stamina = ns.bladeburner.getStamina()[1] / ns.bladeburner.getStamina()[0] * 100;
+	while (true) {
+		let stamina = ns.bladeburner.getStamina()[0] / ns.bladeburner.getStamina()[1] * 100;
 		let chaos = ns.bladeburner.getCityChaos(ns.bladeburner.getCity());
 		let rank = ns.bladeburner.getRank();
-		let nextblackop = ""
 
-		for (const name of blackops) {
-			if (blackops[name].rank <= rank && ns.bladeburner.getActionSuccesses("Black Ops", name) > 0) {
-				nextblackop = name;
+		// Figure out what BlackOp is next up
+		let nextblackop = ""
+		for (const blackop of ns.bladeburner.getBlackOpNames()) {
+			if (ns.bladeburner.getBlackOpRank(blackop) <= rank && ns.bladeburner.getActionCountRemaining("BlackOps", blackop) > 0) {
+				nextblackop = blackop;
 				break;
 			}
 		}
 
-		let skillpoints = ns.bladeburner.getSkillPoints();
+		// Buy the cheapest skills until we can't afford any more
 		let skillnames = ns.bladeburner.getSkillNames();
-		let nextskill = {name: skillnames[0], cost: ns.bladeburner.getSkillUpgradeCost(skillnames[0])};
-		for (const skill of skillnames) {
-			let cost = ns.bladeburner.getSkillUpgradeCost(skill);
-			if (cost < nextskill.cost) {
-				nextskill = {name: skill, cost: cost};
+		while (true) {
+			let skillpoints = ns.bladeburner.getSkillPoints();
+			let nextskill = { name: skillnames[0], cost: ns.bladeburner.getSkillUpgradeCost(skillnames[0]) };
+			for (const skill of skillnames) {
+				let cost = ns.bladeburner.getSkillUpgradeCost(skill);
+				if (cost < nextskill.cost) {
+					nextskill = { name: skill, cost: cost };
+				}
 			}
+			// If we can afford the skill, buy it
+			if (skillpoints >= nextskill.cost) {
+				ns.bladeburner.upgradeSkill(nextskill.name)
+			} else {
+				break;
+			}
+			await ns.sleep(1);
 		}
 
-		if (skillpoints >= nextskill.cost) {
-			ns.bladeburner.upgradeSkill(nextskill.name)
-		} else if (stamina < 75) {
-			ns.bladeburner.startAction("General", "Hyperbolic Regeneration Chamber");
-		} else if (chaos > 100) {
-			ns.bladeburner.startAction("General", "Diplomacy");
-		} else if (nextblackop != "") {
-			ns.bladeburner.startAction("Black Ops", nextblackop);
-			await ns.sleep(ns.bladeburner.getActionTime("Black Ops", nextblackop) + 2000)
+		// Let's actually do an action now
+		if (stamina < 75) {
+			await doAction("General", "Hyperbolic Regeneration Chamber")
+		} else if (chaos > 50) {
+			await doAction("General", "Diplomacy")
+		} else if (nextblackop != "" && ns.bladeburner.getActionEstimatedSuccessChance("BlackOps", nextblackop)[0] * 100 > 30) {
+			await doAction("BlackOps", nextblackop)
 		} else {
-			ns.bladeburner.startAction("Contracts", "Retirement")
+			await doAction("Operations", "Assassination")
 		}
 
-		await ns.sleep(100)
+		await ns.sleep(100);
 	}
-
+	async function doAction(actionType, actionName) {
+		ns.bladeburner.startAction(actionType, actionName);
+		await ns.sleep(ns.bladeburner.getActionTime(actionType, actionName));
+	}
 }
